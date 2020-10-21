5dafe56763fcbfd4af65bb7d2e9542fefdbb1f17
wallermatt <waller.matthew.alt@gmail.com>
2020-10-21 22:24:58 +0100

    bubble working

--- bubble.asm
+++ bubble.asm
@@ -1,13 +1,11 @@
 ; bubble sort
-a_temp_loc=100
-b_temp_loc=101
-d_temp_loc=102
 swap_flag_loc=103
-swap_flag_value=0
+false=0
+true=1
 start_data_loc=120
 end_data_loc=129
 ; load initial
-LD AV,swap_flag_value
+LD AV,false
 LD MA,swap_flag_loc
 LD DV,start_data_loc
 ; loop through data
@@ -16,14 +14,32 @@ main_loop:
 LDI ADM
 ; load b with contents of memory loc d + 1
 INCD
+LD BV,end_data_loc
+GTDB
+JMP RV,end_of_data_reached
 LDI BDM
 ; check if a > b
 GTAB
 JMP RV,swap_values
-JMP RV,end
-LD IM
 JMP V,main_loop
 swap_values:
 ; a -> d, b -> d - 1, set flag
+LDIM DA
+DECD
+LDIM DB
+INCD
+LD AV,true
+LD MA,swap_flag_loc
+JMP V,main_loop
+; when end of data reached, check if any swaps
+end_of_data_reached:
+LD AM,swap_flag_loc
+LD BV,false
+EQAB
+JMP RV,end
+LD MB,swap_flag_loc
+LD DV,start_data_loc
+JMP V,main_loop
+; end of program
 end:
 0
--- run_cpu_2.py
+++ run_cpu_2.py
@@ -2,5 +2,17 @@ from cpu_base import CPUTest
 
 cpu = CPUTest()
 cpu.simple_assembler('bubble.asm')
+
+cpu.memory.set_contents_value(120, 10)
+cpu.memory.set_contents_value(121, 9)
+cpu.memory.set_contents_value(122, 8)
+cpu.memory.set_contents_value(123, 7)
+cpu.memory.set_contents_value(124, 6)
+cpu.memory.set_contents_value(125, 5)
+cpu.memory.set_contents_value(126, 4)
+cpu.memory.set_contents_value(127, 3)
+cpu.memory.set_contents_value(128, 2)
+cpu.memory.set_contents_value(129, 1)
+
 cpu.run()
-print(cpu.memory.dump())
+print(cpu.memory.dump()[120:130])
